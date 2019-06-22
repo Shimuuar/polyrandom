44dcfcf0e4f397236608bfdf33566d1ea603118b
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-06-23 00:11:20 +0300

    Add MLCG for m=2^31 - 1

--- Random/Gen/LCG.hs
+++ Random/Gen/LCG.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE KindSignatures      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
 {-# LANGUAGE UnboxedTuples       #-}
 -- |
 -- Linear congruential generators (LCG). They're defined as recurrence relation:
@@ -14,12 +15,16 @@
 -- If constant @c@ is 0 such generator is called multiplicative linear
 -- congruential generator.
 module Random.Gen.LCG (
+    -- * MLCG
+    MLCG231
+  , MLCG231G(..)
     -- * Reference implementations
-    MLCGRef(..)
+  , MLCGRef(..)
     -- * References
     -- $references
   ) where
 
+import Data.Bits
 import Data.Proxy
 import Data.Word
 import Numeric.Natural
@@ -28,7 +33,44 @@ import GHC.TypeLits
 import qualified Random.PRNG as PRNG
 
 ----------------------------------------------------------------
---
+-- Reference implementation
+----------------------------------------------------------------
+
+-- | MLCG specialized for \[ m = 2^{31}-1 \]. This allows for
+--   efficient computation of remander
+newtype MLCG231G (a :: Nat) = MLCG231 Word32
+  deriving (Show,Eq)
+
+type MLCG231 = MLCG231G 1583458089
+
+stepMLCG231 :: forall a i. (KnownNat a, Integral i) => PRNG.Rand (MLCG231G a) i
+{-# INLINE stepMLCG231 #-}
+stepMLCG231 = PRNG.Rand $ \(MLCG231 w) ->
+  let m31  = 0x7fffffff -- 2^31 - 1
+      a    = fromIntegral $ natVal (Proxy :: Proxy a)
+      prod = fromIntegral w * a :: Word64
+      w'   = case (prod  .&. m31) + (prod `shiftR` 31) of
+               r | r >= m31  -> r - m31
+                 | otherwise -> r
+  in (# MLCG231 (fromIntegral w'), fromIntegral w' #)
+
+instance (1 <= a, a <= 2147483646, KnownNat a) => PRNG.Pure (MLCG231G a) where
+  step32R w     = PRNG.uniformWithRejection 0x7fffffff w stepMLCG231
+  step64R w     = PRNG.uniformWithRejection 0x7fffffff w stepMLCG231
+  -- Derived
+  step32        = PRNG.step32R maxBound
+  step64        = PRNG.step64R maxBound
+  stepFloat01   = withWord32 PRNG.wordToFloat
+  stepFloat01Z  = withWord32 PRNG.wordToFloatZ
+  stepDouble01  = withWord64 PRNG.word64ToDouble
+  stepDouble01Z = withWord64 PRNG.word64ToDoubleZ
+  -- State
+  save    = undefined
+  restore = undefined
+
+
+----------------------------------------------------------------
+-- Reference implementation
 ----------------------------------------------------------------
 
 -- | Reference implementation of multiplicative linear congruential
@@ -71,7 +113,12 @@ instance (KnownNat a, KnownNat m) => PRNG.Pure (MLCGRef a m) where
   -- State
   save    = undefined
   restore = undefined
-  
+
+
+----------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------
+
 withWord32 :: PRNG.Pure g => (Word32 -> a) -> PRNG.Rand g a
 {-# INLINE withWord32 #-}
 withWord32 f = do
